4c084e5b672336fc2341ad610685818337fbeb37
Saurabh Aryal <saurabharyal10@gmail.com>
2026-05-22 08:52:58 +0545

    Initial Commit

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import streamlit as st
 df = sns.load_dataset('penguins')
 st.title('Penguins Data Analysis')
 
-st.write("### Available Columns in Dataset:")
-st.write(list(df.columns))
+# st.write("### Available Columns in Dataset:")
+# st.write(list(df.columns))
 
-fig, ax = plt.subplots()
-sns.scatterplot(data=df, x="bill_length_mm", y="bill_depth_mm", hue="species", ax=ax)
-st.pyplot(fig)
+# fig, ax = plt.subplots()
+# sns.scatterplot(data=df, x="bill_length_mm", y="bill_depth_mm", hue="species", ax=ax)
+# st.pyplot(fig)
