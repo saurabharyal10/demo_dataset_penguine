958ac3759cb156b6c0bbd8507c6e2892bba57fff
Saurabh Aryal <saurabharyal10@gmail.com>
2026-05-22 08:55:01 +0545

    Initial Commit

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 import streamlit as st
-
-df = sns.load_dataset('penguins')
-st.title('Penguins Data Analysis')
-
-# st.write("### Available Columns in Dataset:")
-# st.write(list(df.columns))
-
-# fig, ax = plt.subplots()
-# sns.scatterplot(data=df, x="bill_length_mm", y="bill_depth_mm", hue="species", ax=ax)
-# st.pyplot(fig)
+import seaborn as sns
+ 
+st.title("My First Streamlit App")
+st.write("Hello, welcome to my app!")
+ 
+df= sns.load_dataset("tips")
+ 
+a = sns.relplot(x="total_bill", y="tip", data=df)
+ 
+st.pyplot(a)
